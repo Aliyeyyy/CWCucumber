76aff64757cf99211b40c58b16f73f4690fe1eb1
Aliyeyyy <aliyeyyy35@gmail.com>
2023-01-11 21:29:06 +0100

    push

---
+++ src/test/java/runners/ParallelRunner.java
@@ -0,0 +1,19 @@
+package runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+public class ParallelRunner {
+
+    @RunWith(Cucumber.class)
+    @CucumberOptions(
+            plugin = {
+                    "html:target/report-parallel/report.html"
+            },
+            features = "src/test/resources/features",
+            glue = {"stepdefinitions", "hooks"}
+    )
+    public class ParallelRunner {
+    }
+}
